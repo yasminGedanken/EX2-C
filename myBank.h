2a9eae5937e7dcfcdb0026565790f998b6eac8ed
OmerK25 <48291816+OmerK25@users.noreply.github.com>
2019-11-24 17:16:29 +0200

    Add files via upload

--- myBank.h
+++ myBank.h
@@ -1,9 +1,10 @@
+
 int openAccount(double firstDep);
 int checkIfOpen(int account_number);
 double checkMoney(int accNumber);
 double addMoney(int accNumber, double amount);
 double takeMoney(int accNumber, double amount);
-double tooPoorForOurBankBitch(int accNumber);
-void freeMoneyBitch(double interest_rate);
+double tooPoorForOurBank(int accNumber);
+void freeMoney(double interest_rate);
 void printAll();
 void closeAll();
